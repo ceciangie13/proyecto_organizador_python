8df0b0c53030d6184ba51d5757fa3f8b582b477c
Angie Bermudez <ceci-angie@hotmail.com>
2025-11-15 16:50:40 -0500

    Estructura inicial del proyecto y creación de módulos

---
+++ Codigo/entrada_datos.py
@@ -0,0 +1,13 @@
+#No tiene parámetros porque recibe datos del usuario
+
+#Definir los días a la semana disponibles y cada uno qué horas está disponible
+def tiempo_disponible():
+    return
+
+#Aquí se ingresaran las actividades, si están predefinidas, recursos necesarios
+def actividades():
+    return  
+
+#Definir la cantidad de personas disponibles, registro de identificación con nombre, su disponibilidad
+def personas_disponibles():
+    return
---
+++ Codigo/validaciones.py
@@ -0,0 +1,8 @@
+def disponibilidad_personas():
+    return
+
+def actividades_por_dia():
+    return
+
+def dispoibilidad_recursos():
+    return
